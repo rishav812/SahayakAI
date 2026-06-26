ff2c6f908ef7adc0824f0c57d1f77f1c377beb4a
ritika <ritikaju9@gmail.com>
2026-06-26 09:06:43 +0530

    working on admission agent

--- app/graph.py
+++ app/graph.py
@@ -1,4 +1,4 @@
-from langgraph.graph import MessagesState
+from langgraph.graph import MessagesState,StateGraph,START,END
 from langgraph.prebuilt import ToolNode
 from typing import Annotated, TypedDict
 from langgraph.graph.message import add_messages
@@ -14,8 +14,30 @@ def agent_node(state: AgentState) -> dict:
     response = llm_with_tools.invoke(state["messages"])
     return {"messages": [response]}
 
-
 tool_node = ToolNode([get_fee])
 
+def tool_condition(state:AgentState) -> dict:
+    last_message=state["messages"][-1]
+    if last_message.tool_calls:
+        return "tools"
+    return END
 
 
+workflow=StateGraph(AgentState)
+
+workflow.add_node("Agent",agent_node)
+workflow.add_node("tools",tool_node)
+workflow.add_edge("START","Agent")
+
+workflow.add_conditional_edges(
+    "agent",
+    tool_condition,
+)
+
+admissions_agent = workflow.compile()
+result = admissions_agent.invoke(
+    {"messages": [{"role": "user", "content": "JEE 2-year ki fees?"}]}
+)
+
+for m in result["messages"]:
+    m.pretty_print()
